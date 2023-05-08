b4206657c45e745a11b696ca0e088b4a0e673d9c
Giacomo Della Peruta <giax.95@live.it>
2023-05-08 18:05:04 +0200

    Fixed Routes

--- Front/rocket-cinema/src/App.js
+++ Front/rocket-cinema/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../src/components/Login.jsx";
+import Home from "../src/components/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/buytickets" element={<BuyTickets />} /> */}
+        {/* <Route path="/checkout" element={<Checkout />} /> */}
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
